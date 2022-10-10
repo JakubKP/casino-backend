0a6cbddbea7626e1673b5a5b6ef93330941f89ea
Jakub <73079474+JakubKP@users.noreply.github.com>
2022-10-10 12:13:49 +0200

    Update server.js
    Update cors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000
 
 connectDB()
 
-app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }))
+app.use(cors({ credentials: true, origin: ['https://for-fun-casino.onrender.com'] }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
 app.use(cookieParser())
@@ -32,7 +32,7 @@ const server = app.listen(port, () => {
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000'
+        origin: 'https://for-fun-casino.onrender.com'
     }
 })
 
